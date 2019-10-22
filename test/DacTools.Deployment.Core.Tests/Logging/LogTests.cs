219c1a0c571e2fcab0bad3d4953da3cbae7febcc
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2019-10-22 22:59:20 +0100

    test: added DacTools.Deployment.Core.Tests project to the solution
    Added two files with tests for the Log and LogExtensions classes.
    
    This commit also adds test.msbuild a file that will set common PackageReferences between all of the eventual test projects. The versions for these packages are stored centrally in Directory.Build.props.

---
+++ test/DacTools.Deployment.Core.Tests/Logging/LogTests.cs
@@ -0,0 +1,68 @@
+﻿using DacTools.Deployment.Core.Logging;
+using Shouldly;
+using System;
+using Moq;
+using Xunit;
+
+namespace DacTools.Deployment.Core.Tests.Logging
+{
+	public class LogTests
+	{
+		[Fact]
+		public void ShouldConstructWithAnEmptyArrayOfLogAppenders()
+		{
+			var log = new Log();
+			log.LogAppenders.ShouldBe(Array.Empty<ILogAppender>());
+			log.LogLevel.ShouldBe(LogLevel.Info);
+		}
+
+		[Fact]
+		public void ShouldConstructWithTheSuppliedArrayOfLogAppenders()
+		{
+			var consoleAppender = new ConsoleAppender();
+			var log = new Log(consoleAppender);
+			log.LogAppenders.ShouldBe(new[] {consoleAppender});
+			log.LogLevel.ShouldBe(LogLevel.Info);
+		}
+
+		[Fact]
+		public void ShouldCallWriteToOnAllLogAppenders()
+		{
+			var mockLogAppender = new Mock<ILogAppender>();
+			var log = new Log(mockLogAppender.Object);
+			log.Write(LogLevel.Info, "Message");
+			mockLogAppender.Verify(m => m.WriteTo(LogLevel.Info, It.Is<string>(s => s.Length == 36)), Times.Once);
+		}
+
+		[Fact]
+		public void ShouldAddANewLogAppender()
+		{
+			var consoleAppender = new ConsoleAppender();
+			var log = new Log();
+			log.AddLogAppender(consoleAppender);
+			log.LogAppenders.ShouldBe(new[] {consoleAppender});
+		}
+
+		[Fact]
+		public void ShouldReturnStringBuilderContentsWhenToStringIsCalled()
+		{
+			var log = new Log();
+			log.Write(LogLevel.Info, "Message");
+
+			string result = log.ToString();
+			result.ShouldNotBeNull();
+			result.Length.ShouldBe(36); // Expected Length of 1 Log Message
+		}
+
+		[Fact]
+		public void ShouldReturnWithoutWritingWhenLogLevelIsNotActive()
+		{
+			var log = new Log {LogLevel = LogLevel.Warn};
+			log.Write(LogLevel.Info, "Message");
+
+			string result = log.ToString();
+			result.ShouldNotBeNull();
+			result.Length.ShouldBe(0);
+		}
+	}
+}
