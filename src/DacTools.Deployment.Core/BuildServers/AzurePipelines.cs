d92fb8f6cb7de4a6e21170b45c8134083990d222
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2019-10-23 20:45:12 +0100

    feat(deployment): started the groundwork for Build Server detection
    This includes features such as identifying which build server the tool is running on if any, and then it will provide additional functionality if available. This could be things such as reporting progress or reporting issues during deployments.

---
+++ src/DacTools.Deployment.Core/BuildServers/AzurePipelines.cs
@@ -0,0 +1,31 @@
+﻿using DacTools.Deployment.Core.BuildServers.Common;
+using DacTools.Deployment.Core.Logging;
+
+namespace DacTools.Deployment.Core.BuildServers
+{
+	public class AzurePipelines : BuildServerBase
+	{
+		public AzurePipelines(ILog log) : base(log)
+		{
+		}
+
+		public const string EnvironmentVariableValue = "TF_BUILD";
+
+		protected override string EnvironmentVariable { get; } = EnvironmentVariableValue;
+
+		public override string GenerateSetProgressMessage(int current, int total, string message) =>
+			$"##vso[task.setprogress value={(int) (current / (double) total * 100.0f)};] {message}";
+
+		public override string GenerateLogIssueWarningMessage(string issueMessage) =>
+			$"##vso[task.logissue type=warning;] {issueMessage}";
+
+		public override string GenerateLogIssueErrorMessage(string issueMessage) =>
+			$"##vso[task.logissue type=error;] {issueMessage}";
+
+		public override string GenerateSetStatusSucceededWithIssuesMessage(string statusMessage) =>
+			$"##vso[task.complete type=SucceededWithIssues;] {statusMessage}";
+
+		public override string GenerateSetStatusFailMessage(string statusMessage) =>
+			$"##vso[task.complete type=Failed;] {statusMessage}";
+	}
+}
---
+++ src/DacTools.Deployment.Core/BuildServers/Common/BuildServerBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DacTools.Deployment.Core.Logging;
+
+namespace DacTools.Deployment.Core.BuildServers.Common
+{
+	public abstract class BuildServerBase : IBuildServer
+	{
+		protected readonly ILog Log;
+
+		protected BuildServerBase(ILog log)
+		{
+			Log = log;
+		}
+
+		protected abstract string EnvironmentVariable { get; }
+
+		public virtual bool CanApplyToCurrentContext() =>
+			!string.IsNullOrEmpty(Environment.GetEnvironmentVariable(EnvironmentVariable));
+
+		public abstract string GenerateSetProgressMessage(int current, int total, string message);
+		public abstract string GenerateLogIssueWarningMessage(string issueMessage);
+		public abstract string GenerateLogIssueErrorMessage(string issueMessage);
+		public abstract string GenerateSetStatusSucceededWithIssuesMessage(string statusMessage);
+		public abstract string GenerateSetStatusFailMessage(string statusMessage);
+	}
+}
