e69584f11eae736e125a95ec8d3fb14b8e305f05
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2019-10-25 22:15:35 +0100

    test(deployment): added tests for BuildServerList

---
+++ test/DacTools.Deployment.Core.Tests/BuildServers/BuildServerListTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using DacTools.Deployment.Core.BuildServers;
+using DacTools.Deployment.Core.Logging;
+using DacTools.Deployment.Core.Tests.TestInfrastructure;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace DacTools.Deployment.Core.Tests.BuildServers
+{
+	public class BuildServerListTests
+	{
+		[Theory]
+		[InlineData("True", 1)]
+		[InlineData(null, 0)]
+		public void GetApplicableBuildServersShouldReturnCorrectNumberOfValues(string variableValue, int expectedCount)
+		{
+			// Setup
+			var environment = new TestEnvironment();
+			var log = new Mock<ILog>().Object;
+			BuildServerList.Init(environment, log);
+
+			environment.SetEnvironmentVariable("TF_BUILD", variableValue);
+
+			// Act
+			var result = BuildServerList.GetApplicableBuildServers(log).ToList();
+
+			// Assert
+			result.ShouldNotBeNull();
+			if (expectedCount == 1) result.ShouldNotBeEmpty();
+			result.Count.ShouldBe(expectedCount);
+		}
+	}
+}
